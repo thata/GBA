b09e8af335cea9b0ba1039118ab7f3e96094b373
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2022-01-02 16:52:12 +0900

    Use c preprosessor

--- dots/dots.s
+++ dots/dots.s
@@ -1,13 +1,13 @@
-# My first GBA video drawing program
+// My first GBA video drawing program
 
 .arm
 .text
 
-  mov r1, #0x04000000 @ Set I/O base address
-  ldr r2, =0xF03      @ Set video mode 3
-  strh r2, [ r1 ]     @ Update display control register
-  mov r3, #0x06000000 @ Set VRAM frame buffer address
-  ldr r4, =0x7FFF     @ RGB = 1F/1F/1F --> white
+  mov r1, #0x04000000 // Set I/O base address
+  ldr r2, =0xF03      // Set video mode 3
+  strh r2, [ r1 ]     // Update display control register
+  mov r3, #0x06000000 // Set VRAM frame buffer address
+  ldr r4, =0x7FFF     // RGB = 1F/1F/1F --> white
   strh r4, [ r3 ]
   strh r4, [ r3, #4 ]
   strh r4, [ r3, #8 ]
